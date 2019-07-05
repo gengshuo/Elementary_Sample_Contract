68e16b42b051e71f1994f707fba589414cb60b79
Brown Jhang <leaveslife@gmail.com>
2019-07-05 09:52:08 +0800

    Updates SimpleStorage.sol for storing string data.

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.5.0;
 
 contract SimpleStorage {
-    uint storedData;
+    string storedData;
 
-    function set(uint x) public {
+    function set(string memory x) public {
         storedData = x;
     }
 
-    function get() public view returns (uint) {
+    function get() public view returns (string memory) {
         return storedData;
     }
 }
